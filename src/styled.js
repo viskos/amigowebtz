d72c2f2c3c60199f8f36735528e9fb61f164b03d
viskos <viskoos.fd@gmail.com>
2021-01-24 14:58:05 +0300

    change font family

--- src/styled.js
+++ src/styled.js
@@ -4,7 +4,7 @@ const GlobalStyles = createGlobalStyle`
     * {
         margin: 0;
         padding: 0;
-        font-family: Ubuntu;
+        font-family: 'IBM Plex Sans', sans-serif;
         max-width: 460px;
         width: 100%;
         margin: 0 auto;
